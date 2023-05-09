6a90573d3d1746c921b23e57a6693dd03123115d
ZengDa0501 <smirnovvvasyl@gmail.com>
2023-05-09 09:22:08 +0000

    dev: added router

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Home } from './pages/index';
 
 export const Routers = () => {
   return (
-    <div className=''>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
